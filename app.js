af8de7ff4ba42b23a34536444eba5562a1672e68
GIMENAGASPAR <gimenagaspar7@gmail.com>
2023-08-17 00:25:49 -0300

    resolucion de errores y agregado de app.js con rutas

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express=require("express");
+const fs=require("fs");
+
+const app=express();
+const publicPath=path.resolve(__dirname, "./public");
+const port=process.env.PORT || 3001;
+
+app.use(express.static(publicPath));
+// app.set("puerto", process.env.PORT || 3001)
+
+/* app.listen(3000, ()=>{
+    console.log("servidor corriendo en el puerto 3000");
+}); */
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.listen(port, ()=> console.log('servidor corriendo en el puerto ${port}'));
